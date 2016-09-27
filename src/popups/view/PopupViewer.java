d8b3991010aea1a66498e18120cc188fd2465a5c
kmart22 <kmar7070@740s-pl-111.local>
2016-09-27 08:35:26 -0600

    creates a input popup to collect a response from the user as a string.

--- src/popups/view/PopupViewer.java
+++ src/popups/view/PopupViewer.java
@@ -12,4 +12,17 @@ public class PopupViewer
 	{
 		JOptionPane.showMessageDialog(null, message);
 	}
+	/**
+	 * creates an input popup to collect a response from the user as a string .
+	 * @param question the question being asked to the user.
+	 * @return the users response
+	 */
+	public String collectResponse(String question)
+	{
+		String response = "";
+		
+		response = JOptionPane.showInputDialog(null, question);
+				
+		return response;
+	}
 }
