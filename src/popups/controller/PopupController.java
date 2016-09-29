f8e82e8322d4d89b05ed8ed77c146db35e40e936
kmart22 <kmar7070@740s-pl-111.local>
2016-09-29 08:25:13 -0600

    created a try/catch IO thingy.

--- src/popups/controller/PopupController.java
+++ src/popups/controller/PopupController.java
@@ -26,5 +26,26 @@ public class PopupController
 		}
 	
 	}
-
+	
+	
+	/**
+	 * checks if the supplied string can be parsed to a double value.
+	 * @param potentialValue the string to test
+	 * @return whether the value could be parsed; true f it can be parsed, false otherwise
+	 */
+	private boolean isDouble(String potentialValue)
+	{
+		      boolean isParseable = false;
+		      
+		      try
+		      {
+		    	  double test = Double.parseDouble(potentialValue);
+		    	  isParseable = true;
+		      }
+		      catch(NumberFormatException notDouble)
+		      {
+		    	  display.displayMessage("this is not a double value =:<");
+		      }
+		      return isParseable;
+	}
 }
